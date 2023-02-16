233c57fdcba83e1b751482cb94e730dc3edcb84c
jeeun99 <wpdms27@gmail.com>
2023-02-16 17:51:13 +0900

    2023.02.16 version

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 var swiper = new Swiper(".slide1", {
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".next1",
+    prevEl: ".prev1",
   },
   autoplay: {
-    delay: 2500,
+    delay: 3000,
     disableOnInteraction: false,
   },
 
