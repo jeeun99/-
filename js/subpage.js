b28a5739045c5350c2b5575f20e26b1573b9ee81
jeeun99 <wpdms27@gmail.com>
2023-02-17 17:55:54 +0900

    2023.02.17

---
+++ js/subpage.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".slide2", {
+  autoplay: {
+    delay: 6000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+});
+var swiper = new Swiper(".slide3", {
+  autoplay: {
+    delay: 6000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+});
